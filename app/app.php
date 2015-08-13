c72ef0f17e28d8d80f588a654cf0501816622f6c
Alex Costa & Annie Hoogendoorn <student@epicodus.com>
2015-08-13 13:58:13 -0700

    Add boiler plate to the app.php file

---
+++ app/app.php
@@ -0,0 +1,15 @@
+<?php
+
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Scrabble.php';
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    // Home twig link
+
+    return $app;
+?>
